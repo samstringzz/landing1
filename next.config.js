fa428fed39c290531403389985ea3d7b9c37f83c
samstringzz <freetlessguitarist@gmail.com>
2025-09-09 23:01:53 +0100

    update

--- next.config.js
+++ next.config.js
@@ -7,6 +7,27 @@ module.exports = {
     unoptimized: true,
   },
   webpack: (config) => {
+    // Remove any existing CSS rules
+    config.module.rules = config.module.rules.filter(
+      (rule) => !(rule.test && rule.test.test('.css'))
+    );
+
+    // Add our own CSS rules
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        'next/dist/compiled/css-loader',
+        {
+          loader: 'postcss-loader',
+          options: {
+            postcssOptions: {
+              plugins: ['postcss-preset-env'],
+            },
+          },
+        },
+      ],
+    });
+
     config.resolve.fallback = { fs: false };
     return config;
   }
