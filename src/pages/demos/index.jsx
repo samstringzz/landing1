570b50c98e227fe806131c2da0b00f1c7ba4f91b
samstringzz <freetlessguitarist@gmail.com>
2025-09-09 21:56:58 +0100

    updates

--- src/pages/demos/index.jsx
+++ src/pages/demos/index.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Link from "next/link";
+import "../../styles/demos.css";
 import Split from "../../components/Split";
 import DarkTheme from "../../layouts/Dark";
 import addParlx from "../../common/addParlx";
@@ -40,87 +41,6 @@ const Demos = () => {
 
   return (
     <DarkTheme>
-      <style jsx>
-        {`
-          .masonery .item-img {
-            position: relative;
-            overflow: hidden;
-          }
-
-          .masonery .gallery .items {
-            padding: 0 40px;
-            margin-top: 80px;
-            text-align: center;
-          }
-
-          .masonery .gallery .items h6 {
-            margin-top: 25px;
-            font-size: 17px;
-            font-weight: 400;
-            font-family: "Barlow Condensed", sans-serif;
-            text-align: center;
-            text-transform: uppercase;
-            letter-spacing: 3px;
-          }
-
-          .masonery .gallery .items .item-img:hover img {
-          }
-
-          .masonery .item-img:hover .tlinks {
-            opacity: 1;
-          }
-
-          .masonery .item-img .tlinks {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            transition: all 0.4s;
-            background: rgba(0, 0, 0, 0.5);
-            opacity: 0;
-            padding: 0 40px;
-          }
-
-          .masonery .item-img .tag{
-              position: absolute;
-              top: 15px;
-              right: 15px;
-              padding: 8px 20px;
-              border-radius: 5px;
-              background: #ff0036;
-              color: #fff;
-              font-size: 13px;
-          }
-
-          .masonery .item-img .tlinks a {
-            padding: 14px 20px;
-            border-radius: 5px;
-            background: #32363e;
-            color: #fff;
-            font-size: 14px;
-            font-weight: 500;
-            margin: 5px 0;
-          }
-
-          .masonery .item-img .tlinks a:last-of-type {
-            background: #75dab4;
-            color: #181b21;
-            margin-left: 5px;
-          }
-
-          .masonery .item-img .new {
-            padding: 10px 30px;
-            background: #75dab4;
-            position: absolute;
-            top: 0;
-            left: -60px;
-            width: 200px;
-            transform: rotate(-30deg);
-            font-size: 13px;
-          }
-        `}
-      </style>
 
       <header
         ref={fixedHeader}
